6dc947819c3797445e2ad9b2724df5be96086178
CRD <support@2chat.co>
2023-09-04 11:27:13 -0600

    Initial code commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# WhatsApp number checker
+
+This project uses [2Chat's](https://2chat.co?ref=gh) public API to verify if a given phone number has a WhatsApp account.
+
+It takes as input a CSV file containing the numbers to check, and it outputs another CSV file with the results returned by the API.
+
+For more information about the API in use here, please check [2Chat's developer portal](https://developers.2chat.co/docs/API/WhatsApp/check-number/?ref=gh).
+
+## Requisites
+
+* A 2Chat account. You can create one [here](https://app.2chat.io/signup).
+
+* A WhatsApp number connected to your account. You can learn how to connect one [here](https://help.2chat.io/en/articles/7243195-how-to-create-a-whatsapp-channel?ref=gh).
+
+* Your 2Chat API key. Learn where to obtain it [here](https://help.2chat.io/en/articles/7830948-where-you-can-find-the-api-code-in-2chat?ref=gh).
+
+## How to use the script
+
+This is a Node script written in Javascript.
+
+To run it, clone this repository and:
+
+* Create a file named `.env` with your API key in it:
+```
+API_KEY=<your API key here>
+```
+
+* Install the dependencies:
+```
+$ npm install
+```
+
+* Create a file with the numbers you want to check. For example, `my-numbers-to-check.csv` and use the following format:
+
+```
++17137157533
++525511223344
++31206490787
+```
+
+* Call the script with the configured parameters:
+
+```
+Options:
+  --help                     Show help                                 [boolean]
+  --version                  Show version number                       [boolean]
+  --input-file, --in         Input file containing the list of numbers you want
+                             to verify                       [string] [required]
+  --output-file, --out       Output file where the script will append the result
+                             of each number verification     [string] [required]
+  --source-number, --number  The number connected to 2Chat you want to use to
+                             run this script and perform the number
+                             verifications                   [string] [required]
+```
+
+`--number` must be the number you connected to your 2Chat account. For example, `+17137157533`.
+
+```
+$ node check-number.js --in=my-numbers-to-check.csv --out=verification-results.csv --number=+17137157533
+```
+
+* Check the output file for the results:
+
+```
+$ cat verification-results.csv
+```
+
+## API limits and running this script in parallel
+
+We recommend you run a single instance of this script for every connected number on your 2Chat account to achieve faster speeds.
+
+Your account may be rate-limited during the trial period.
+
+## Support
+
+If you encounter issues or have any problems, please reach out to our support channels at [2Chat.co](https://2chat.co?ref=gh).
---
+++ src/check-number.js
@@ -0,0 +1,170 @@
+#!/usr/bin/env node
+
+//
+// WhatsApp number verification script using 2Chat's public API
+// 
+// Author: 2Chat Team <support@2chat.co>
+// For information at:
+//     - https://github.com/2ChatCo/whatsapp-number-checker
+//     - https://developers.2chat.co/docs/API/WhatsApp/check-number
+//  
+//
+
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const fs = require('fs');
+const csv = require('csv-parser');
+const axios = require('axios');
+const path = require('path');
+const csvWriter = require('csv-write-stream');
+
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+const args = yargs(hideBin(process.argv))
+    .option('input-file',
+        {
+            alias: 'in',
+            type: 'string',
+            description: 'Input file containing the list of numbers you want to verify'
+        }
+    )
+    .option('output-file',
+        {
+            alias: 'out',
+            type: 'string',
+            description: 'Output file where the script will append the result of each number verification'
+        }
+    )
+    .option('source-number',
+        {
+            alias: 'number',
+            type: 'string',
+            description: 'The number connected to 2Chat you want to use to run this script and perform the number verifications'
+        }
+    )
+    .demandOption('input-file')
+    .demandOption('output-file')
+    .demandOption('source-number')
+    .argv;
+
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+} 
+
+async function main() {
+    if (!fs.existsSync(args.in)) {
+        throw new Error(`Input file [${args.in}] couldn't be found`);
+    }
+
+    if (!fs.existsSync(".env")) {
+        throw new Error(`Environment file [.env] couldn't be found`);
+    }
+
+    if (!process.env.API_KEY) {
+        throw new Error(`API_KEY entry is missing in .env file`);
+    }
+
+    const numbersToCheck    = [];
+    let outputWriter        = null;
+
+    if (!fs.existsSync(args.out)) {
+        outputWriter = csvWriter({ headers: ["phone_number", "on_whatsapp", "json_number_information", "json_whatsapp_information"]});
+    }
+    else {
+        outputWriter = csvWriter({sendHeaders: false});
+    }
+
+    fs.createReadStream(args.in)
+        .pipe(csv({headers: false, skipLines: 1}))
+        .on('data', async (row) => {
+
+            //
+            // column 0 must always be the phone number
+            //
+            const phoneNumber = row[0];
+            
+            if (!phoneNumber)
+                return;
+
+            numbersToCheck.push(phoneNumber);
+        })
+        .on('end', async () => {
+            try {
+                console.log('CSV file successfully processed. Will check numbers now');
+                
+                outputWriter.pipe(fs.createWriteStream(args.out, { flags: 'a' }));
+
+                for (const index in numbersToCheck) {
+                    verifyNumberAndSave(numbersToCheck[index], outputWriter);
+
+                    //
+                    // we wait 5 seconds to respect the API limits or the request will fail
+                    //
+                    await sleep(5000);
+                }
+            }
+            catch (e) {
+                console.error(e);
+            }
+            finally {
+                outputWriter.end();
+            }
+
+        }); 
+}
+
+function verifyNumberAndSave(numberToVerify, csvWriter) {
+    console.log(`Trying to verify number=[${numberToVerify}] using source=[${args.number}]`);
+
+    var config = {
+        method: 'GET',
+        url:    `https://api.p.2chat.io/open/whatsapp/check-number/${args.number}/${numberToVerify}`,
+        headers: { 
+            'X-User-API-Key': process.env.API_KEY
+        }
+    };
+
+    axios(config)
+        .then(function (response) {
+            try {
+                const result = response.data;
+
+                console.log(`${numberToVerify}:`, result);
+                
+                csvWriter.write({
+                    phone_number: numberToVerify,
+                    on_whatsapp: result?.on_whatsapp,
+                    json_number_information: JSON.stringify(result?.number),
+                    json_whatsapp_information: JSON.stringify(result?.whatsapp_info),
+                });
+            }
+            catch (e) {
+                console.log(e);
+            }
+        })
+        .catch(function (error) {
+            try {
+                if (!error?.response) {
+                    console.error(e);    
+                }
+                else {
+                    console.error(`API error: status=[${error.response?.status}] reason=[${error.response?.statusText}]:`, error.response?.data);
+                    
+                    //
+                    // Fatal errors
+                    //
+                    if (error.response.status === 401 || error.response.status === 402 || error.response.status === 404) {
+                        process.exit(-1);
+                    }
+
+                    console.log(`Checking number=${numberToVerify} failed. Please retry later.`)
+                }
+            }
+            catch (e) {
+                console.error(e);
+            }
+            
+        });
+}
+
+main();
